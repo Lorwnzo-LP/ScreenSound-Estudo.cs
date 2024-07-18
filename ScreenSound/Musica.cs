d299b0c9f2b7395696db0a9801f486f56b0c46cd
Lorenzo Loss <lorenzocontato1@outlook.com>
2024-07-18 15:56:51 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,27 @@
+﻿class Musica
+{
+    public string Nome    { get; set; }
+    public string Artista { get; set; }
+    public int Duracao    { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}";
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome/Artista: {Nome} - {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else {
+            Console.WriteLine("Adquira com o plano tal");
+        }
+    }
+    
+}
+
+
+
